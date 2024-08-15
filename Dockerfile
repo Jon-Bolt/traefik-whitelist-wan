ea36ca68573f473decca2bc1e3cea88ee18eb1c5
Jonathan Bolt <Jon-Bolt@users.noreply.github.com>
2024-08-15 01:55:10 +0000

    update to leverage some better python code on timothymiller/cloudflare-ddns

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,18 @@
-FROM python:3.9-slim-buster
+# ---- Base ----
+FROM python:3.9-slim-buster AS base
 
-WORKDIR /app
+#
+# ---- Dependencies ----
+FROM base AS dependencies
+# install dependencies
+COPY requirements.txt .
+RUN pip install --user -r requirements.txt
 
+#
+# ---- Release ----
+FROM base AS release
+# copy installed dependencies and project source file(s)
+WORKDIR /
+COPY --from=dependencies /root/.local /root/.local
 COPY get_wan_ip.py . 
-
-RUN pip install requests
-
-# Add the CMD instruction to run your script
-CMD ["python", "get_wan_ip.py"] 
+CMD ["python", "-u", "/cloudflare-ddns.py", "--repeat"]
--- get_wan_ip.py
+++ get_wan_ip.py
@@ -1,21 +1,35 @@
-import requests
-import os
-import time
-import logging
+#!/usr/bin/env python3
 
-# Configure the logger
-logging.basicConfig(level=logging.INFO, format='%(asctime)s | %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+import json
+import os
+import signal
+import sys
+import threading
+import time
+import requests
+
+CONFIG_PATH = os.environ.get('CONFIG_PATH', os.getcwd())
+
+class GracefulExit:
+    def __init__(self):
+        self.kill_now = threading.Event()
+        signal.signal(signal.SIGINT, self.exit_gracefully)
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+    def exit_gracefully(self, signum, frame):
+        print("🛑 Stopping main thread...")
+        self.kill_now.set()
 
 def get_wan_ip():
     try:
         response = requests.get('https://api.ipify.org')
-        response.raise_for_status()  # Check for HTTP errors
+        response.raise_for_status()
         return response.text
     except requests.exceptions.RequestException as e:
-        print(f"Error fetching WAN IP: {e}")
+        print(f"😡 Error fetching WAN IP: {e}")
         return None
 
-def write_ip_to_file(ip_address, filename="ip-allow-middleware.yml"):
+def write_yaml_to_file(ip_address, filename="ip-allow-middleware.yml"):
     config_folder = "/config"
     if not os.path.exists(config_folder):
         os.makedirs(config_folder)
@@ -28,27 +42,49 @@ def write_ip_to_file(ip_address, filename="ip-allow-middleware.yml"):
         sourceRange:
           - "{ip_address}"
         ipStrategy: 
-          depth: 1""" 
+          depth: 1"""
 
     try:
         with open(file_path, "w") as f:
             f.write(yaml_content)
-        logging.info(f"Updated {file_path}")
+        print(f"✅ Updated {file_path}")
     except IOError as e:
-        logging.error(f"Error writing to file: {e}") 
+        print(f"😡 Error writing to file: {e}")
 
-if __name__ == "__main__":
-    interval_seconds = int(os.environ.get('UPDATE_INTERVAL_SECONDS', 3600))
-    previous_ip = None  # Store the previous IP address
+def update_ip(previous_ip, last_change_time):
+    global last_unchanged_hour 
 
-    while True:
-        wan_ip = get_wan_ip()
-        if wan_ip and wan_ip != previous_ip:
-            write_ip_to_file(wan_ip)
-            previous_ip = wan_ip  # Update the previous IP
+    wan_ip = get_wan_ip()
+    if wan_ip and wan_ip != previous_ip:
+        write_yaml_to_file(wan_ip)
+        return wan_ip, time.localtime()
+    else:
+        if not wan_ip:
+            print("⚠️ Skipping update due to API error. Retrying in next interval.")
         else:
-            if not wan_ip:
-                logging.warning("Skipping update due to API error. Retrying in next interval.")
-            else:
-                logging.debug("WAN IP hasn't changed. Skipping update.")
-        time.sleep(interval_seconds)
+            current_hour = time.localtime().tm_hour
+            if current_hour != last_unchanged_hour:
+                last_change_formatted = time.strftime("%Y-%m-%d %H:%M:%S", last_change_time) if last_change_time else "N/A"
+                print(f"ℹ️ WAN IP hasn't changed (last change: {last_change_formatted}). Skipping update.")
+                last_unchanged_hour = current_hour 
+        return previous_ip, last_change_time
+
+if __name__ == '__main__':
+    if sys.version_info < (3, 5):
+        raise Exception("🐍 This script requires Python 3.5+")
+
+    interval_seconds = int(os.environ.get('UPDATE_INTERVAL_SECONDS', 3600))
+    previous_ip = None
+    last_unchanged_hour = None
+    last_change_time = None
+
+    if (len(sys.argv) > 1 and sys.argv[1] == "--repeat"):
+        print(f"🕰️ Updating YAML file every {interval_seconds} seconds")
+        next_time = time.time()
+        killer = GracefulExit()
+        while True:
+            previous_ip, last_change_time = update_ip(previous_ip, last_change_time)
+            if killer.kill_now.wait(interval_seconds):
+                break
+    else:
+        update_ip(previous_ip, last_change_time)
